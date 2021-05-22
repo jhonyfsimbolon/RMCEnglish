5a84b640bf8dcf01d84536d34e6caa9074d027de
jhonyfsimbolon <62797725+jhonyfsimbolon@users.noreply.github.com>
2021-05-22 19:09:12 +0700

    Manual upload
    donlod manual ae

---
+++ RMCEnglish/app/src/main/java/com/ekrol/mobile/rmcenglish/BagianA.java
@@ -0,0 +1,14 @@
+package com.ekrol.mobile.rmcenglish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BagianA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bagian_a);
+    }
+}
